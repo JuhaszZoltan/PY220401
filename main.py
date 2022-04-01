1f76ddbeb23b051d4de9c0f908d5ff7ccc74d377
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-04-01 12:55:48 +0200

    updt

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from module import IdozitettFelirat, get_lista, leghosszabb_felirat
 
 idozitett_feliratok = get_lista('feliratok.txt')
 
-# print(f'5. feladat - feliratok száma: {len(idozitett_feliratok)}')
-# print('7. feladat - legtöbb szóból álló felirat:')
-# print(leghosszabb_felirat(idozitett_feliratok))
-
-for x in idozitett_feliratok:
-    print(x.srt_idozites)
+print(f'5. feladat - feliratok száma: {len(idozitett_feliratok)}')
+print('7. feladat - legtöbb szóból álló felirat:')
+print(leghosszabb_felirat(idozitett_feliratok))
--- module.py
+++ module.py
@@ -11,9 +11,6 @@ class IdozitettFelirat:
         ep = ep if eo != '01' else f'{"0" if int(ep)-60 <= 10 else ""}{int(ep)-60}'
         vp = vp if vo != '01' else f'{"0" if int(vp)-60 <= 10 else ""}{int(vp)-60}'
         self.srt_idozites = f'{eo}:{ep}:{e_v[0].split(":")[1]} --> {vo}:{vp}:{e_v[1].split(":")[1]}'
-        # eleje_vege[0] = f'00:{eleje_vege[0]}' if int(eleje_vege[0].split(':')[0]) < 60 else f'01:0{int(eleje_vege[0].split(":")[0]) - 60}:{eleje_vege[0].split(":")[1]}'
-        # eleje_vege[1] = f'00:{eleje_vege[1]}' if int(eleje_vege[1].split(':')[0]) < 60 else f'01:0{int(eleje_vege[1].split(":")[0]) - 60}:{eleje_vege[1].split(":")[1]}'
-        # self.srt_idozites = f'{eleje_vege[0]} --> {eleje_vege[1]}'
 
 
 def get_lista(f:str) -> list[IdozitettFelirat]:
